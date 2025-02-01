98ee2b0959e1e8eee6d8a4730b1f5ee777c56d9b
athulkrishnaph <athulhassi@gmail.com>
2025-02-02 00:03:58 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://chat-app-server-qky4.onrender.com");
 
 function App() {
   const [message, setMessage] = useState("");
